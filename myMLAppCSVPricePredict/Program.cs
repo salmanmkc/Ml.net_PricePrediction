52f76dab7fba2b546b130a6a63bd17d4229dac53
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-05-27 12:56:53 +0100

    added more data and checked with input data, and it is really close! With more time to train I can get a more accurate model

--- myMLAppCSVPricePredict/Program.cs
+++ myMLAppCSVPricePredict/Program.cs
@@ -22,9 +22,9 @@ namespace myMLAppCSVPricePredict
 
             // Use the code below to add input data
             var input = new ModelInput();
-            input.Trip_distance = 2;
+            input.Trip_distance = 1.3f;
             input.Passenger_count = 1;
-            input.Trip_time_in_secs = 1099;
+            input.Trip_time_in_secs = 385;
             //input.Payment_type = "CRD":
 
             // Try model on sample data
@@ -37,8 +37,9 @@ namespace myMLAppCSVPricePredict
               Distance: 3     Passenger Count: 3      Trip time (s): 1099     Price:  14.11699
               Distance: 3     Passenger Count: 1      Trip time (s): 1099     Price:  14.11699
               Distance: 2     Passenger Count: 1      Trip time (s): 1099     Price:  12.59949
-
-
+              Distance: 2     Passenger Count: 1      Trip time (s): 800      Price:  10.5829   
+              Distance: 1.3f  Passenger Count: 1      Trip time (s): 385      Price:  6.830118, this is good since actual data has 1.3f, 1,385 as an output of 7
+              
 
              */
         }
