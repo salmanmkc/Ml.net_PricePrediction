32a747433b5c3d29ed48a50cd5ee508b99b67b9f
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-05-27 12:37:08 +0100

    Add project files.

---
+++ myMLAppCSVPricePredict/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myMLAppCSVPricePredict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
